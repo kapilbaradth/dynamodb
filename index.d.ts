99c961d9d43aba2e9efc2e51b2938f53dba581c8
Mick Lawitzke <info@lawitzke.de>
2020-04-17 17:25:08 +0200

    Typings WIP

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,27 @@
 import { AnySchema } from '@hapi/joi';
+import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
 
 export interface SchemaConfig {
   hashKey: string;
   rangeKey?: string;
   timestamps: boolean;
   schema: {
-    roles: any;
+    roles?: any;
     [key:string]: AnySchema
-  }
+  };
 }
+
+export interface DynamoDB {
+  type: any;
+  AWS: any;
+  documentClient(docClient: DocumentClient);
+  define(modelName: string, config: SchemaConfig): any;
+  createTables();
+} 
 
 export function define(modelName: string, config: SchemaConfig): any;
 export var types: any;
+export var AWS;
+export function documentClient(docClient: DocumentClient);
+
+// export namespace dynamodb;
